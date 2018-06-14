1a634a85b739828220b92d865490f6f8c1cc0d29
blackbird1128 <alexbulle@orange.fr>
2018-06-14 14:09:59 +0200

    add a method to set the enters

--- layer.hpp
+++ layer.hpp
@@ -47,11 +47,58 @@ public:
 			
 
 	}
+	template<typename T>
+	void Compute(T ActivationFunction)
+	{
+		for (auto i = 0; i < neuronLayer.size(); i++)
+		{
 
 
+			auto result = neuronLayer[i].Compute(ActivationFunction);
+			exit.push_back(result);
+		}
+
+
+	}
+	void SetEnter(Layer previousLayer)
+	{
+		for (auto j = 0; j < previousLayer.exit.size(); j++)
+		{
+			for (auto i = 0; i < neuronLayer.size(); i++)
+			{
+
+				neuronLayer[i].Enter.push_back(previousLayer.exit[j]);
+				neuronLayer[i].SetWeight();
+			}
+           
+
+		}
+
+
+
+	}
+	void SetEnter(std::vector<float> EntertoSet) // hum name is bad 
+	{
+        
+		if (EntertoSet.size() != neuronLayer.size())
+		{
+			throw std::string{ "Enter size != neuronLayer size " };
+		}
+		for (auto i = 0; i < EntertoSet.size(); i++)
+		{
+
+			neuronLayer[i].Enter.push_back(EntertoSet[i]);
+			neuronLayer[i].SetWeight();
+		}
+
+			
+	
+
+	}
 
 	std::vector<neuron> neuronLayer;
-
+	std::vector<float> exit;
+	std::vector<float> enter;
 
 
 
