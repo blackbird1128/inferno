eb875b81a861b9fefbf28f2d44643f7450a8feb0
blackbird1128 <alexbulle@orange.fr>
2018-06-20 16:52:11 +0200

    add usefuls mathematicals functions

--- mathematicalFunction.hpp
+++ mathematicalFunction.hpp
@@ -1,6 +1,8 @@
 #pragma once
 #include <iostream>
 #include <cmath>
+#include <vector>
+#include <stdexcept>
 
 
 
@@ -8,10 +10,10 @@
 
 
 
-int Heaviside(float x)// no idea of name 
+int Heaviside(double x)
 {
 
-	int result;
+	int result = 0 ;
 	if (x  < 0)
 	{
 		result = 0;
@@ -30,7 +32,7 @@ int Heaviside(float x)// no idea of name
 
 
 
-double Sigmoide(double x)
+double Sigmoid(double x)
 {
 	double exp_value;
 	double return_value;
@@ -44,6 +46,12 @@ double Sigmoide(double x)
 
 
 }
+double SigmoidPrime(double x)
+{
+
+	return Sigmoid(x) * (1 - Sigmoid(x));
+
+}
 
 
 float ComputeCost(float resultDesired, float ResultGet)
@@ -52,6 +60,38 @@ float ComputeCost(float resultDesired, float ResultGet)
 	cost = resultDesired - ResultGet;
 	return cost;
 }
+
+
+template<typename T>
+T Dot(std::vector<T> vec1, std::vector<T> vec2)
+{
+	if (vec1.size() != vec2.size())
+	{
+		throw std::length_error(std::string("Both arrays must have the same length"));
+
+
+	}
+	double  sum = 0;
+	for (auto i = 0; i < vec1.size(); i++)
+	{
+
+		sum += (vec1[i] * vec2[i]);
+	}
+	T result = sum;
+	return result;
+
+}
+
+std::vector<int> ToOneHot(int index, int range)
+{
+
+	std::vector<int> oneHotvector(range, 0);
+	oneHotvector[index] = 1;
+	return oneHotvector;
+
+
+}
+
 
 
 
